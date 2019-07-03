a0a9094a8c42fb80459d15b42c004c5c2dbac5ca
luciala2028 <51397053+luciala2028@users.noreply.github.com>
2019-07-03 14:25:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-if (userText == 'สวัสดี') :
+    if (userText == 'สวัสดี') :
        sendText(user,'ไอ้บ้า')
     elif (userText == 'น่ารัก') :
     sendText(user,'บ้าบอ')
